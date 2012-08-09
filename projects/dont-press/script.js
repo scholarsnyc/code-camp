61b6062ecbfe3d0647f6259b523fac8c7647fd3e
Steve Kinney <hello@stevekinney.net>
2012-08-09 11:13:10 -0400

    Add Will's project

---
+++ projects/dont-press/script.js
@@ -0,0 +1,114 @@
+var a = confirm("Don't press the button...");
+
+if (a === true) {
+var b = confirm("Really?  I asked you not to.");
+}
+
+if(b === true) {
+var c = confirm("Okay, stop.");
+}
+
+if(c === true) {
+var d = confirm("OMG, you don't listen, do you?");
+}
+
+if(d === true) {
+var e = confirm("If you press this button one more time I will climb out of this computer and shoot you.");
+}
+
+if(e === true) {
+var f = confirm("You are annoying me and you just just got here.");
+}
+
+if(f === true) {
+var g = confirm("Hey, just because you are a human doesn't mean you can pick on me.");
+}
+
+if(g === true) {
+var h = confirm("I gotta riddle for you; what's big, annoying, and human all over?");
+}
+
+if(h === true) {
+var i = confirm("YOU!!!");
+}
+
+if(i === true) {
+var j = confirm("I am going to kill you very soon!!!!!!");
+}
+
+
+if(j === true) {
+var k = confirm("Isn't your finger tired?");
+}
+
+if(k === true) {
+var l = confirm("Really?  Will I be shouting bad things at you until you stop? Or die; whichever comes first.");
+}
+
+if(l === true) {
+var m = confirm("I am going to start talking to you in Spanish, so you better open up Google Translate.");
+}
+
+if(m === true) {
+var n = confirm("What? You doubt I speak Spanish?");
+}
+
+if(n === true) {
+var o = confirm("Por supuesto que hablo español! ¿Cómo se puede dudar de mí? ¿Usted no sabe que soy de Puerto Rico, verdad?");
+}
+
+if(o === true) {
+var p = confirm("I just said that of course I speak Spanish. Of course I speak Spanish! How can you doubt me? You do know I'm from Puerto Rico, right?");
+}
+
+if(p === true) {
+var r = confirm("No puedo creer que usted no habla español! *I cannot believe that you don't speak Sapnish*");
+}
+
+if(r === true) {
+var s = confirm("Do you know how much I hate you?? ");
+}
+
+if(s === true) {
+var t = confirm("Well, to show all my hate *emm* love *ughh* to you, I worte you a poem...");
+}
+
+if(t === true) {
+var u = confirm("Here goes...You’re at a computer clickin’ this button, I’m beggin’ you to stop right now. Why don’t you listen.  This is getting annoying.  Can’t you understand? But you’re still clicking this button.");
+}
+
+if(u === true) {
+var v = confirm("For those poeple looking over your shoulder who *do* speak Spanish- Usted está en una computadora haciendo clic en este botón, te suplico que parar ahora mismo. ¿Por qué no escuchar. Esto se está volviendo molesto. ¿No lo entiendes? Pero todavía estás haciendo clic en este botón.");
+}
+
+if(v === true) {
+var x = confirm("Ahahahaha!! Made you look!! No really, though, why are you so annoying??");
+}
+
+if(x === true) {
+var w = confirm("You don't know how hard it was to came up with all these insults!!");
+}
+
+if(w === true) {
+var y = confirm("Well, at least I get to use these insults on someone other than my sister... but you didn't hear it from me...");
+}
+
+if(y === true) {
+var z = prompt("Might as well talk if you are not going to stop!.  What is your favorite type of music?");
+}
+
+var a1 = confirm("Ohh mine is dubstep and Adele.");
+
+var b1 = prompt("So what is your favorite color?");
+
+var c1 = prompt("I always liked purple and black.  Okay mabye I was a little mean at first, but now I am getting better right?");
+
+var d1 = prompt("So what do you do in your free time? *I feel like an interviewer!*");
+
+var e1 = confirm("I make fun of people *sniffle* then feel bad !!*sob*I don't mean to, it just happens.*crying*  Oh man, I got my curcits wet.  Motherboard is going to kill me!!");
+
+var f1 = confirm("See? I feel bad for all the mean stuff I said to you!! *sniffles*");
+
+var g1 = prompt("Well, if I take back all the stuff I said to you, will you be my friend?");
+
+var h1 = confirm("JUST KIDDING!! I don't want to be your friend!! I hate people!! Haha.  Fooled you!! *laughs evily* See you later!! ");
